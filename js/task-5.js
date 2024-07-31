7dfb88bf725f77806b224eaa6866e8c2eccfcd69
aliso4ka <gulyayevaalisa@gmail.com>
2024-07-31 17:06:08 +0200

    task 5 ready

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,14 @@
+const btnChange = document.querySelector(".change-color");
+const bodyColor = document.querySelector("body");
+const spanText = document.querySelector(".color");
+
+btnChange.addEventListener("click", (event) => {
+  const newColor = getRandomHexColor();
+  bodyColor.style.backgroundColor = newColor;
+  spanText.textContent = newColor;
+  
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
