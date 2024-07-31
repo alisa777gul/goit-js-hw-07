d1112733568a2adab5dd65a15589a2684c1b89f0
aliso4ka <gulyayevaalisa@gmail.com>
2024-07-31 16:56:18 +0200

    ready task 4:

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const btnSubmit = document.querySelector("button");
+const form = document.querySelector("form");
+const datas = {  
+};
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    
+    const email = form.elements.email.value.trim();
+    const password = form.elements.password.value.trim();
+    if (email === "" || password === "") {
+        alert('All form fields must be filled in');
+    } else {
+        datas.email = email;
+        datas.password = password;
+        console.log(datas);
+        form.reset();
+    }
+    
+});
 
